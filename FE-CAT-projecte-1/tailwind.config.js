4707a0ae4539851d6f2e93c7ce2967c08047c2d2
Irene Sanchez <irenealpes@gmail.com>
2022-10-26 21:17:24 +0200

    enver with tailwindcss

---
+++ FE-CAT-projecte-1/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./index.html", "./input.css"],
+  theme: {
+    extend: {
+      fontFamily: {
+        nunito: ["Nunito Sans"]
+      },
+      colors: {
+        'irene' : '#19191B',
+        'irene2' : '#1E1E20'
+      },
+    },
+    
+  },
+  plugins: [],
+};
